d3368ef7c52eead00dac3afd2e17e8e2cdfa5e76
JesusSilvaEsquinas <jesus.silva1@insbaixcamp.cat>
2025-11-19 16:56:53 +0100

    incluir els estils

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,6 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>CI/CD Laravel</title>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 
 <body class="antialiased bg-neutral-950 text-neutral-100">
