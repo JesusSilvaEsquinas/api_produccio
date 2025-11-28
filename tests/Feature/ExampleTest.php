6dca6f3bd5c969dce50beee90a5117fc8884a33c
JesusSilvaEsquinas <jesus.silva1@insbaixcamp.cat>
2025-11-28 17:05:02 +0100

    falloCI

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -1,7 +1,16 @@
 <?php
 
-it('returns a successful response', function () {
-    $response = $this->get('/');
+namespace Tests\Unit;
 
-    $response->assertStatus(200);
-});
+use PHPUnit\Framework\TestCase;
+
+class ExampleTest extends TestCase
+{
+    /**
+     * A basic test example.
+     */
+    public function test_that_true_is_true(): void
+    {
+         $this->assertTrue(false);
+    }
+}
